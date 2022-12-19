3d76e2baf5c359498dfbb4623bd7d3354a311f00
muskan altaf <muskanaltaf116@gmail.com>
2022-12-19 13:19:31 +0500

    m

---
+++ task10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<<"           --------------------------------------------------   "<<endl;
+cout<<"                                                                "<<endl; 
+cout<<"                     o     ^   ^                                "<<endl; 
+cout<<"                           ____                                 "<<endl; 
+cout<<"                        o  (oo)\\___________                     "<<endl; 
+cout<<"                           (__)\\           )\\/\\                 "<<endl; 
+cout<<"                                ||-----w   |                    "<<endl; 
+cout<<"                                ||         ||                   "<<endl; 
+}
